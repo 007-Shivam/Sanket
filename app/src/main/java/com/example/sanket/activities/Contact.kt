1504c39c1099bbc79b32ead5bc697f7df3d7cc95
ayushisingh65 <ayushisingh652019@gmail.com>
2024-01-16 21:36:41 +0530

    contact us

--- app/src/main/java/com/example/sanket/activities/Contact.kt
+++ app/src/main/java/com/example/sanket/activities/Contact.kt
@@ -1,14 +1,211 @@
 package com.example.sanket.activities
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import  androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.sanket.R
+
 
 @Composable
 fun Contact(navController: NavHostController) {
-    Text(text = "Contact")
+
+    Box() {
+        Image(
+            painter = painterResource(id = R.drawable.bg),
+            contentDescription = "background",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .matchParentSize()
+        )
+
+        Text(
+            text = stringResource(id = R.string.contact),
+            style = TextStyle(
+                fontSize = 40.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                color = Color(94, 48, 35),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(id = R.string.content),
+            style = TextStyle(
+                fontSize = 26.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                color = Color(94, 48, 35),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 80.dp),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(id = R.string.customer),
+            style = TextStyle(
+                fontSize = 26.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                color = Color(94, 48, 35),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 350.dp),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(id = R.string.cust_email),
+            style = TextStyle(
+                fontSize = 26.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                color = Color(94, 48, 35),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 380.dp),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(id = R.string.tech),
+            style = TextStyle(
+                fontSize = 26.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                color = Color(94, 48, 35),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 440.dp),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(id = R.string.tech_email),
+            style = TextStyle(
+                fontSize = 26.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                color = Color(94, 48, 35),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 470.dp),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(id = R.string.business),
+            style = TextStyle(
+                fontSize = 26.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                color = Color(94, 48, 35),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 530.dp),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(id = R.string.business_email),
+            style = TextStyle(
+                fontSize = 26.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                color = Color(94, 48, 35),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 560.dp),
+            textAlign = TextAlign.Center
+        )
+
+
+        Text(
+            text = stringResource(id = R.string.social_media),
+            style = TextStyle(
+                fontSize = 26.sp,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold,
+                color = Color(94, 48, 35),
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 630.dp),
+            textAlign = TextAlign.Center
+        )
+        Image(
+            painter = painterResource(id = R.drawable.instagram1),
+            contentDescription = "logo",
+            modifier = Modifier
+                .padding(top = 690.dp, bottom = 20.dp, start = 60.dp)
+                .size(width = 26.dp, height = 26.dp)
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.Start),
+        )
+        Image(
+            painter = painterResource(id = R.drawable.linkedin1),
+            contentDescription = "logo",
+            modifier = Modifier
+                .padding(top = 690.dp, bottom = 20.dp, start = 140.dp)
+                .size(width = 26.dp, height = 26.dp)
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.Start),
+        )
+        Image(
+            painter = painterResource(id = R.drawable.twitter),
+            contentDescription = "logo",
+            modifier = Modifier
+                .padding(top = 690.dp, bottom = 20.dp, start = 220.dp)
+                .size(width = 26.dp, height = 26.dp)
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.Start),
+        )
+        Image(
+            painter = painterResource(id = R.drawable.whatsapp),
+            contentDescription = "logo",
+            modifier = Modifier
+                .padding(top = 690.dp, bottom = 20.dp, start = 300.dp)
+                .size(width = 26.dp, height = 26.dp)
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.Start),
+        )
+
+
+    }
 }
 
 @Preview
